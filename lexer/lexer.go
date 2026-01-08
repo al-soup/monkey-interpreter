08a2afeca122ee05d97c059785c40ff14c945244
al-soup <al@soup.one>
2026-01-08 11:26:31 +0100

    feat: tokenize a simple program

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,8 @@
 package lexer
 
-import token "github.com/al-soup/monkey-interpreter/token"
+import (
+	token "github.com/al-soup/monkey-interpreter/token"
+)
 
 type Lexer struct {
 	input        string
@@ -11,24 +13,97 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
-
+	l.readChar()
 	return l
 }
 
 func (l *Lexer) NextToken() token.Token {
-	return token.Token{
-		Type:    "foo",
-		Literal: "bar",
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			// advancing to next read char is handled in `readIdentifier` so we return early
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
 	}
 }
 
 // give next character and position in input
 func (l *Lexer) readChar() {
+	// Check for end of input
 	if l.readPosition >= len(l.input) {
-		l.ch = 0
+		l.ch = 0 // ASCII code for 'NUL'
 	} else {
 		l.ch = l.input[l.readPosition]
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) readIdentifier() string {
+	identStartPosition := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[identStartPosition:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
